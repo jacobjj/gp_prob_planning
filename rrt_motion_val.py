8718aaa7c0d2d678d747fbff2a53790c6e030760
jacobjj <jjj025@ucsd.edu>
2020-10-12 22:35:41 -0700

    added motion model

--- rrt_motion_val.py
+++ rrt_motion_val.py
@@ -15,6 +15,56 @@ except ImportError:
     print("Could not f  ind OMPL")
     raise ImportError("Run inside docker!!")
 
+# Import project specific files
+from models import point
+from gp_model import get_model
+
+# Set up environment details
+thresh = 0.001
+N = stats.norm(scale=np.sqrt(1/2))
+c = N.ppf(1-thresh)
+
+obstacles, robot = point.set_env()
+
+# Define the GP model
+m = get_model(robot, obstacles, point)
+# Define LTI system
+A, B = point.get_dyn()
+
+fig, ax = plt.subplots(1,2)
+sns.set()
+
+# TODO: Set up the plotting code.
+
+K = m.kern.K(m.X)
+prior_var = 1e-6
+# TODO : There is a better way to this inverse!!
+K_inv = np.linalg.inv(K+np.eye(K.shape[0])*prior_var)
+weights = K_inv@ m.Y
+k_x_x = m.kern.K(np.c_[0,0])
+
+def get_GP_G(start, goal):
+    '''
+    The guassian function from x_start and x_goal.Return the function for line
+    :param start:  A np.array representing the start position
+    :param goal: A np.array representing the goal position
+    :return function: Return the function of F along the line
+    '''
+    def G(alpha, *args):
+        '''
+        The function G
+        '''
+        if alpha<0:
+            alpha=0
+        elif alpha>1:
+            alpha = 1
+        x_hat = (1-alpha)*start + alpha*goal
+        k_star = m.kern.K(x_hat, m.X)
+        var = k_x_x-k_star@K_inv@k_star.T
+        return ((k_star@weights)/np.sqrt(2*var))[0]
+    return G
+
+
 class check_motion(ob.MotionValidator):
     '''A motion validator check using chance constrained.
     '''
