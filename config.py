26c18ebf36133c0d595b9d39043ca7bd8f078c3b
jacobjj <jjj025@ucsd.edu>
2020-10-19 11:42:55 -0700

    added parameters for box

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Box obstacles
 xy = [np.r_[2,2], np.r_[2, 8], np.r_[5,5], np.r_[8, 2], np.r_[8, 8]] # Box positions
+box_length, box_width  = 1., 1.
 
 # Circle Obstacles
 cir_radius = 0.5
