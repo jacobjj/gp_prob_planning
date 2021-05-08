38a0289dfbd960e563807b79595b4e294d9cf23a
jacobjj <jjj025@ucsd.edu>
2021-05-08 16:38:01 -0700

    Code cleanup

--- models/racecarv2.py
+++ models/racecarv2.py
@@ -146,57 +146,13 @@ def initialize_constraints(client_obj, car):
                         childFramePosition=[0, 0, 0])
     client_obj.changeConstraint(c, gearRatio=-1, gearAuxLink=15, maxForce=10000)
 
-seed = 10
-np.random.seed(seed)
-# # Randomly generate boxes
-# num_boxes = 8
-# xy = np.random.rand(num_boxes, 2)*9 + 0.5
-
-# # Randomly generate circles
-# num_circles = 5
-# xy_circle = np.random.rand(num_circles, 2)*9 + 0.5
-
-# def set_obstacles(client_obj):
-#     '''
-#     A function to set obstacles in the environment
-#     :param client_obj: A pybullet_utils.BulletClient object
-#     '''
-#     rgba = [0.125, 0.5, 0.5, 1]
-#     geomBox = client_obj.createCollisionShape(pyb.GEOM_BOX, halfExtents=[box_length/2, box_width/2, 0.2])
-#     visualBox = client_obj.createVisualShape(pyb.GEOM_BOX, halfExtents=[box_length/2, box_width/2, 0.2], rgbaColor=rgba)
-#     geomCircle = client_obj.createCollisionShape(pyb.GEOM_CYLINDER, radius=cir_radius, height = 0.4)
-#     visualCircle = client_obj.createVisualShape(pyb.GEOM_CYLINDER, radius=cir_radius, length = 0.4, rgbaColor=rgba)
 
 
-#     obstacles_box = [
-#         client_obj.createMultiBody(
-#             baseMass=0,
-#             baseCollisionShapeIndex=geomBox,
-#             baseVisualShapeIndex=visualBox,
-#             basePosition=np.r_[xy_i, 0.2]
-#         ) 
-#         for xy_i in xy
-#         ]
-
-#     obstacles_circle = [
-#         client_obj.createMultiBody(
-#             baseMass=0,
-#             baseCollisionShapeIndex=geomCircle,
-#             baseVisualShapeIndex=visualCircle,
-#             basePosition=np.r_[xy_i, 0.2]
-#         ) 
-#         for xy_i in xy_circle
-#     ]
-#     obstacles = obstacles_box + obstacles_circle
-#     return obstacles
-
-
-
-
-def set_env(name='Random'):
+def set_env(name='Random', **kwargs):
     '''
     Set the environment as either Random or Gibson
-    :param name: Either has to be 'Random' or 'Gibson'
+    :param name: Either has to be 'Random' or 'Gibon'
+    :param kwargs: Other environment parameters
     :return tuple: The pybullet ID of obstacles and robot
     '''
     assert name in ['Random', 'Gibson'], 'Only two environments possible Gibson/Random'
@@ -230,7 +186,7 @@ def set_env(name='Random'):
         NoiseParams.N_t = stats.multivariate_normal(cov=NoiseParams.N)
         NoiseParams.modelName = f'gibson'
 
-    obstacles = set_obstacles(p)        
+    obstacles = set_obstacles(p, **kwargs)
     return obstacles, car
 
 def check_collision(x, obstacles, robot):
